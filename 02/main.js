83c5847eb0fb06c93718cd0abd324780c1fdb6bb
Irene Huang <irene12332170@gmail.com>
2021-04-21 23:43:05 +0800

    02,05

--- 02/main.js
+++ 02/main.js
@@ -1,13 +1,22 @@
-$(function () {
-    $("input").on("click", function () {
-        //alert("Hi");
+var i1 = 0;
+
+function move() {
+    i1++;
+    if (i1 > 2) i1 = 0;
+    $("h1").text($("li").eq(i1).text());
+}
+run1 = window.setInterval("move()", 250);
+
+$(function() {
+    $("input").on("click", function() {
+        clearInterval(run1);
         var numberOfListItem = $("li").length;
-        var randomChildNumber = Math.floor(Math.random() * numberOfListItem);//Math.floor取整數,Math.random()為0-1的小數
+        var randomChildNumber = Math.floor(Math.random() * numberOfListItem); //Math.floor取整數,Math.random()為0-1的小數
         $("h1").text($("li").eq(randomChildNumber).text());
         var myimages = new Array()
         myimages[0] = "0.jpg"
         myimages[1] = "1.jpg"
         myimages[2] = "2.jpg"
-        document.getElementById("myImage").src = myimages[randomChildNumber] ;
+        document.getElementById("myImage").src = myimages[randomChildNumber];
     });
 });
